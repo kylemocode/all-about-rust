7af8423ccd912d7881fa67f954f0d27b1c6f220e
kylemocode <kylemo860617@gmail.com>
2021-07-18 14:36:46 +0800

    add get todos endpoint & code structure refactor

--- todo-actix-web/src/main.rs
+++ todo-actix-web/src/main.rs
@@ -1,21 +1,31 @@
+mod config;
 mod models;
+mod handlers;
+mod db;
 
-use crate::models::Status;
+// use crate::models::Status;
 use actix_web::{HttpServer, App, web, Responder};
 use std::io;
-
-async fn status() -> impl Responder {
-    web::HttpResponse::Ok()
-        .json(Status { status: "OK".to_string() })
-}
+use dotenv::dotenv;
+use tokio_postgres::NoTls;
+use crate::handlers::*;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()>{
-    println!("start server");
 
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap(); // unwrap -> error handling
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!("start server at http://{}:{}/", config.server.host, config.server.port);
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone()) // Make a copy of reference
             .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
     })
     .bind("127.0.0.1:8080")?
     .run()
